349e3560f15d3b1a0ad43c5c2be15b7feab6f691
minhphucvien <vienphuc2511@gmail.com>
2024-04-12 23:18:19 +0700

    feat: add style for children of main layout

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { useCallback, useState } from "react";
 import Header from "./components/Header/Header";
-
+import "./index.css";
 const MainLayout = () => {
     const [sidebarOpened, setSidebarOpened] = useState(false);
 
@@ -13,7 +13,9 @@ const MainLayout = () => {
         <div>
             <Header onClick={toggleSidebar} />
             <Sidebar open={sidebarOpened} onMaskClick={toggleSidebar} />
-            <Outlet />
+            <div className="children-style">
+                <Outlet />
+            </div>
         </div>
     );
 };
